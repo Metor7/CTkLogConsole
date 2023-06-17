8b7136fb9a7c079b87b1be4ccdc865934be2e46d
Metor <78621101+Metor7@users.noreply.github.com>
2023-06-17 15:43:21 +0200

    Added all files.
    All the files I made earlier have been added.

---
+++ CTkLogsConsole/__init__.py
@@ -0,0 +1,11 @@
+"""
+CustomTkinter Table widget
+Author: Akash Bora
+License: MIT
+This is a custom table widget for customtkinter.
+Homepage: https://github.com/Akascape/CTkTable
+"""
+
+__version__ = '0.2'
+
+from .ctk_log_terminal_CTK import CTkConsole
